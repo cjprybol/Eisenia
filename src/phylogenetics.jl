0c173126722274d8ffe9d3f65567a633ed949dcb
Cameron Prybol <cameron.prybol@gmail.com>
2020-11-11 18:09:33 +0000

    Add more updates

--- src/phylogenetics.jl
+++ src/phylogenetics.jl
@@ -5,32 +5,48 @@
 # dna/rna should require a kmer
 
 # fasta -> distance matrix
-
 # list of fastas -> distance matrix
 
-# identifiers + distance matrix -> tree
+# sanitize protein identifiers
+# should grab this from fasta & join id + description
+# may make sanitize a true false option, I want it for compatability with dendroscope
+sequence_names = map(row -> join([replace(row["protein_name"], r"[^\w\d\._-]" => ""), row["phage"], row["genus"]], "-"), collect(DataFrames.eachrow(phage_table)))
 
-# tree + identifiers -> newick
+function determine_distance(s1, s2, score_model)
+    alignment = BioAlignments.pairalign(BioAlignments.GlobalAlignment(), s1, s2, score_model)
+    mean_length = Statistics.mean([length(s1), length(s2)])
+    length_normalized_score = alignment.value/mean_length
+    # negative scores get higher distances
+    # possible distances range from (0 - Inf)
+    d = 1/MathConstants.e^(length_normalized_score)
+end
 
-score_model = BioAlignments.AffineGapScoreModel(BioAlignments.BLOSUM45, gap_open=0, gap_extend=0)
+# allow user to pass score model | just distance matrix 
+score_model = 
+BioAlignments.AffineGapScoreModel(
+    BioAlignments.BLOSUM45, 
+    gap_open=minimum(BioAlignments.BLOSUM45.data), 
+    gap_extend=Statistics.median(BioAlignments.BLOSUM45.data))
+
+
 distance_matrix = zeros(Float64, DataFrames.nrow(phage_table), DataFrames.nrow(phage_table))
 ProgressMeter.@showprogress for i in 1:DataFrames.nrow(phage_table)
     for j in i+1:DataFrames.nrow(phage_table)
         s1 = phage_table[i, "protein_sequence"]
         s2 = phage_table[j, "protein_sequence"]
-        d = 1/BioAlignments.pairalign(BioAlignments.GlobalAlignment(), s1, s2, score_model).value
+        d = determine_distance(s1, s2, score_model)
         distance_matrix[i, j] = distance_matrix[j, i] = d
     end
 end
 distance_matrix
 
-sequence_names = map(row -> join([replace(row["protein_name"], r"[^\w\d\._-]" => ""), row["phage"], row["genus"]], "-"), collect(DataFrames.eachrow(phage_table)))
-
-# this is equivalent to UPGMA
+# distance matrix -> tree
 tree = Clustering.hclust(distance_matrix, linkage=:average, branchorder=:optimal)
-return identifiers, tree, distance matrix
 
-
+# tree + identifiers -> newick
+# take identifiers, tree, make newick
+# use fasta + newick as default name
+# allow user passable name
 newick = Dict()
 for row in 1:size(tree.merges, 1)
     left, right = tree.merges[row, :]
@@ -50,3 +66,9 @@ end
 open("$(DIR)/proteins.myoviridae.newick", "w") do io
     println(io, newick[size(tree.merges, 1)] * ";")
 end
+
+
+
+return identifiers, tree, distance matrix
+
+
